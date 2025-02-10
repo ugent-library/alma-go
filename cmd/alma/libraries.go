60c60451418604a5fde9c7f1596f10bd0f89ab35
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2025-02-10 11:54:37 +0100

    add get libraries

---
+++ cmd/alma/libraries.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(getLibrariesCmd)
+}
+
+var getLibrariesCmd = &cobra.Command{
+	Use:   "libraries",
+	Short: "Get libraries",
+	Args:  cobra.NoArgs,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		resBody, err := almaClient.RawGetLibraries(cmd.Context())
+		if err != nil {
+			return err
+		}
+
+		return writeJSON(cmd, resBody)
+	},
+}
--- libraries.go
+++ libraries.go
@@ -5,6 +5,10 @@ import (
 	"fmt"
 )
 
+func (c *Client) RawGetLibraries(ctx context.Context) ([]byte, error) {
+	return c.rawRequest(ctx, "GET", "/conf/libraries", nil, nil)
+}
+
 func (c *Client) RawGetLibrary(ctx context.Context, id string) ([]byte, error) {
 	return c.rawRequest(ctx, "GET", fmt.Sprintf("/conf/libraries/%s", id), nil, nil)
 }
